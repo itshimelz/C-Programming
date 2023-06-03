49287a7e442e6efbc0f51ee7dc6c6a49460a4aaf
itshimelz <himelhasan1215@gmail.com>
2023-06-03 17:58:14 +0600

    Added solution for long words codeforce problem

--- Codeforces Problems/long_words.c
+++ Codeforces Problems/long_words.c
@@ -5,5 +5,27 @@ int main()
     int n;
     scanf("%d", &n);
 
+    char word[101];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", word);
+
+        int lenght = 0;
+        while (word[lenght] != '\0')
+        {
+            lenght++;
+        }
+
+        if (lenght > 10)
+        {
+            printf("%c%d%c\n", word[0], lenght - 2, word[lenght - 1]);
+        }
+        else
+        {
+            printf("%s\n", word);
+        }
+    }
+
     return 0;
 }
